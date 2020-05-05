6c956b1bb340f2b2dccb0903e35199e6faddfb84
shantovai024 <56038258+shantovai024@users.noreply.github.com>
2020-05-05 12:46:27 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+
+  // Slider Carousel
+  $('.slider-active').owlCarousel({
+    loop:true,
+    nav:true,
+    navText:['<i class="fa fa-angle-left" aria-hidden="true"></i>','<i class="fa fa-angle-right" aria-hidden="true"></i>'],
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+
+ // Slider Carousel
+ $('.testi-active').owlCarousel({
+    loop:true,
+    nav:true,
+    navText:['<i class="fa fa-angle-left" aria-hidden="true"></i>','<i class="fa fa-angle-right" aria-hidden="true"></i>'],
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+
+
+
+
+})
